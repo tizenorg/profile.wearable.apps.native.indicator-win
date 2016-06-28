7458acc9b779d655c81d293848bf4a5c6013cd06
Gowtham Anandha Babu <gowtham.ab@samsung.com>
2016-06-28 23:54:04 +0530

    [W-Indicator] Update to latest UI spec guidelines
    1) Remove music-player, flight-mode control icons
    2) Add volume control icons
    3) Fix BT/WIFI ordering following GUI guidelines
    4) Fix codingcheck errors
    
    Change-Id: I768606ea3be08898488eb81ec89d8562acd438cb

--- src/windicator_brightness.c
+++ src/windicator_brightness.c
@@ -28,131 +28,131 @@
 
 void on_clicked_small_brightness_icon(void *data, Evas_Object *obj, const char *emission, const char *source)
 {
-        _D("on_clicked_small_brightness_icon");
-        struct appdata *ad = (struct appdata *)data;
-        ret_if(ad == NULL);
+	_D("on_clicked_small_brightness_icon");
+	struct appdata *ad = (struct appdata *)data;
+	ret_if(ad == NULL);
 
-        //launch setting
-        ad->launch_setting_trigger = 1;
-        windicator_util_launch_app(ad);
+	//launch setting
+	ad->launch_setting_trigger = 1;
+	windicator_util_launch_app(ad);
 }
 
 void on_pressed_small_brightness_icon(void *data, Evas_Object *obj, const char *emission, const char *source)
 {
-        _D("");
-        struct appdata *ad = (struct appdata *)data;
-        ret_if(ad == NULL);
+	_D("");
+	struct appdata *ad = (struct appdata *)data;
+	ret_if(ad == NULL);
 
-        elm_object_signal_emit(ad->brightness_small_layout, "brightness.pressed", "img.brightness.bg");
+	elm_object_signal_emit(ad->brightness_small_layout, "brightness.pressed", "img.brightness.bg");
 }
 
 void on_released_small_brightness_icon(void *data, Evas_Object *obj, const char *emission, const char *source)
 {
-        _D("");
-        struct appdata *ad = (struct appdata *)data;
-        ret_if(ad == NULL);
+	_D("");
+	struct appdata *ad = (struct appdata *)data;
+	ret_if(ad == NULL);
 
-        elm_object_signal_emit(ad->brightness_small_layout, "brightness.released", "img.brightness.bg");
+	elm_object_signal_emit(ad->brightness_small_layout, "brightness.released", "img.brightness.bg");
 }
 
 Evas_Object *windicator_brightness_small_layout_create(Evas_Object *parent, void *data)
 {
-        retv_if(parent == NULL, NULL);
+	retv_if(parent == NULL, NULL);
 
-        _D("windicator_brightness_small_layout_create");
-        struct appdata *ad = (struct appdata *)data;
-        retv_if(ad == NULL, NULL);
+	_D("windicator_brightness_small_layout_create");
+	struct appdata *ad = (struct appdata *)data;
+	retv_if(ad == NULL, NULL);
 
-        Evas_Object *layout = elm_layout_add(parent);
-        retv_if(layout == NULL, NULL);
+	Evas_Object *layout = elm_layout_add(parent);
+	retv_if(layout == NULL, NULL);
 
-        char full_path[PATH_MAX] = { 0, };
-        _get_resource(EDJ_FILE, full_path, sizeof(full_path));
-        _D("full_path:%s",full_path);
+	char full_path[PATH_MAX] = { 0, };
+	_get_resource(EDJ_FILE, full_path, sizeof(full_path));
+	_D("full_path:%s", full_path);
 
-        if(elm_layout_file_set(layout, full_path, "windicator/brightness/small_layout") != EINA_TRUE) {
-        		_E("Failed to set brightness small layout");
-                evas_object_del(layout);
-                return NULL;
-        }
-        evas_object_size_hint_weight_set (layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	if (elm_layout_file_set(layout, full_path, "windicator/brightness/small_layout") != EINA_TRUE) {
+		_E("Failed to set brightness small layout");
+		evas_object_del(layout);
+		return NULL;
+	}
+	evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 
-        elm_object_part_content_set(parent, "brightness.swallow", layout);
-        /* for press effect */
-        elm_object_signal_callback_add(layout, "mouse,down,1", "img.brightness.icon", on_pressed_small_brightness_icon, ad);
-        elm_object_signal_callback_add(layout, "mouse,up,1", "img.brightness.icon", on_released_small_brightness_icon, ad);
-        evas_object_show(layout);
-        return layout;
+	elm_object_part_content_set(parent, "brightness.swallow", layout);
+	/* for press effect */
+	elm_object_signal_callback_add(layout, "mouse,down,1", "img.brightness.icon", on_pressed_small_brightness_icon, ad);
+	elm_object_signal_callback_add(layout, "mouse,up,1", "img.brightness.icon", on_released_small_brightness_icon, ad);
+	evas_object_show(layout);
+	return layout;
 }
 /*
 static int _brightness_level_to_index(int level, int max)
 {
-        int index = 1;
+	int index = 1;
 
-        if(level <= 1) {
-                index = 1;
-        } else if(level <= 20) {
-                index = 2;
-        } else if(level <= 40) {
-                index = 3;
-        } else if(level <= 50) {
-                index = 4;
-        } else if(level <= 60) {
-                index = 5;
-        } else if(level <= 70) {
-                index = 6;
-        } else if(level <= 80) {
-                index = 7;
-        } else if(level <= 90) {
-                index = 8;
-        } else if(level <= 95) {
-                index = 9;
-        } else if(level <= 100) {
-                index = 10;
-        } else {
-        		_E("Failed to change level to index");
-                index = 1;
-        }
+	if (level <= 1) {
+		index = 1;
+	} else if (level <= 20) {
+		index = 2;
+	} else if (level <= 40) {
+		index = 3;
+	} else if (level <= 50) {
+		index = 4;
+	} else if (level <= 60) {
+		index = 5;
+	} else if (level <= 70) {
+		index = 6;
+	} else if (level <= 80) {
+		index = 7;
+	} else if (level <= 90) {
+		index = 8;
+	} else if (level <= 95) {
+		index = 9;
+	} else if (level <= 100) {
+		index = 10;
+	} else {
+			_E("Failed to change level to index");
+		index = 1;
+	}
 
-        _I("brightness level: %d -> index: %d", level, index);
+	_I("brightness level: %d -> index: %d", level, index);
 
-        return index;
+	return index;
 }
 */
 char *windicator_util_str_from_icu_get(int level)
 {
-         char *ret_str = NULL;
-         char locale[32] = {0,};
+	 char *ret_str = NULL;
+	 char locale[32] = {0,};
 
-		 snprintf(locale, sizeof(locale)-1, "%d", level);
-		 ret_str = strdup(locale);
-		 if(ret_str == NULL) {
-			   _E("Failed to duplicate string");
-			   return NULL;
-		 }
-		 return ret_str;
+	snprintf(locale, sizeof(locale)-1, "%d", level);
+	ret_str = strdup(locale);
+	if (ret_str == NULL) {
+		_E("Failed to duplicate string");
+		return NULL;
+	}
+	return ret_str;
 }
 
 void windicator_brightness_icon_set_by_level(Evas_Object *layout, void *data)
 {
-        ret_if(layout == NULL);
+	ret_if(layout == NULL);
 
-        struct appdata *ad = (struct appdata *)data;
-        ret_if(ad == NULL);
+	struct appdata *ad = (struct appdata *)data;
+	ret_if(ad == NULL);
 
-        char value[1024];
+	char value[11];
 
-        _D("Brightness index, set (%s)", value);
+	_D("Brightness index, set (%s)", value);
 
-		snprintf(value,sizeof(value),"%d",ad->brightness_index);
-		elm_object_part_text_set(layout,"txt.brightness",value);
+	snprintf(value, sizeof(value), "%d", ad->brightness_index);
+	elm_object_part_text_set(layout, "txt.brightness", value);
 }
 
 
 static int _change_bright_level_to_index(int level)
 {
 	int index = 0;
-	_D("level:%d",level);
+	_D("level:%d", level);
 	if (level >= 0 && level <= 100) {
 		index = (level / 10);
 		_D("Setting - level %d -> index : %d", level, index);
@@ -163,39 +163,39 @@ static int _change_bright_level_to_index(int level)
 
 static int _current_device_brightness_level_get(void)
 {
-       int level = 0;
-       int index = 0;
+	int level = 0;
+	int index = 0;
 
-        _D("Outdoor mode, so get VCONFKEY_SETAPPL_LCD_BRIGHTNESS");
-	if(vconf_get_int(VCONFKEY_SETAPPL_LCD_BRIGHTNESS, &level) < 0)
+	_D("Outdoor mode, so get VCONFKEY_SETAPPL_LCD_BRIGHTNESS");
+	if (vconf_get_int(VCONFKEY_SETAPPL_LCD_BRIGHTNESS, &level) < 0)
 		_SECURE_E("Failed to get vconfkey : %s", VCONFKEY_SETAPPL_LCD_BRIGHTNESS);
-	_D("brightness value read from vconf key: VCONFKEY_SETAPPL_LCD_BRIGHTNESS is %d",level);
+	_D("brightness value read from vconf key: VCONFKEY_SETAPPL_LCD_BRIGHTNESS is %d", level);
 	index = _change_bright_level_to_index(level);
-	 _I("Brightness level index : %d", index);
-        return index;
+	_I("Brightness level index : %d", index);
+	return index;
 }
 
 windicator_error_e windicator_brightness_update(void *data)
 {
-        struct appdata *ad = (struct appdata *)data;
-        retv_if(ad == NULL, WINDICATOR_ERROR_INVALID_PARAMETER);
-        /* update icon & slider */
-        ad->brightness_index = _current_device_brightness_level_get();
-        windicator_brightness_icon_set_by_level(ad->brightness_small_layout, ad);
+	struct appdata *ad = (struct appdata *)data;
+	retv_if(ad == NULL, WINDICATOR_ERROR_INVALID_PARAMETER);
+	/* update icon & slider */
+	ad->brightness_index = _current_device_brightness_level_get();
+	windicator_brightness_icon_set_by_level(ad->brightness_small_layout, ad);
 //      windicator_brightness_slider_set_by_level(ad);
 
-        return WINDICATOR_ERROR_OK;
+	return WINDICATOR_ERROR_OK;
 }
 
 void windicator_brightness_small_layout_destroy(void *data)
 {
-        struct appdata *ad = (struct appdata *)data;
-        ret_if(ad == NULL);
+	struct appdata *ad = (struct appdata *)data;
+	ret_if(ad == NULL);
 
-        if(ad->brightness_small_layout != NULL) {
-                _D("Destroy BRIGHTNESS Layout");
-                evas_object_del(ad->brightness_small_layout);
-                ad->brightness_small_layout = NULL;
-        }
+	if (ad->brightness_small_layout != NULL) {
+		_D("Destroy BRIGHTNESS Layout");
+		evas_object_del(ad->brightness_small_layout);
+		ad->brightness_small_layout = NULL;
+	}
 }
 
