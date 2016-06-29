60bb3eddfcba77da3cce68564a1474c393025c5f
Gowtham Anandha Babu <gowtham.ab@samsung.com>
2016-06-29 20:40:21 +0530

    [W-Indicator] Update to latest UI spec guidelines
    1) Remove music-player, flight-mode control icons
    2) Add volume control icons
    3) Fix BT/WIFI ordering following GUI guidelines
    4) Fix codingcheck errors
    
    Change-Id: Ib0bde11e1c69267c5a9396f0ebd73da8e0a7e65e

--- src/windicator_moment_view.c
+++ src/windicator_moment_view.c
@@ -24,180 +24,175 @@
 
 /*Evas_Object *windicator_moment_view_battery_number_create(void *data)
 {
-        struct appdata *ad =(struct appdata *)data;
-        retv_if(ad == NULL, NULL);
+	struct appdata *ad =(struct appdata *)data;
+	retv_if(ad == NULL, NULL);
 
-        _I("moment battery initialize");
-        //int battery_level = 50;
+	_I("moment battery initialize");
+	//int battery_level = 50;
 
-        Evas_Object *layout = elm_layout_add(ad->moment_view_battery_layout);
-        retv_if(layout == NULL, NULL);
+	Evas_Object *layout = elm_layout_add(ad->moment_view_battery_layout);
+	retv_if(layout == NULL, NULL);
 
-        char full_path[PATH_MAX] = { 0, };
-        _get_resource(EDJ_FILE, full_path, sizeof(full_path));
-        _D("full_path:%s",full_path);
+	char full_path[PATH_MAX] = { 0, };
+	_get_resource(EDJ_FILE, full_path, sizeof(full_path));
+	_D("full_path:%s",full_path);
 
-        if(elm_layout_file_set(layout, full_path, "windicator/moment_view/battery/number") != EINA_TRUE) {
-        	_E("Failed to create moment view layout");
-                evas_object_del(layout);
-                return NULL;
-        }
+	if (elm_layout_file_set(layout, full_path, "windicator/moment_view/battery/number") != EINA_TRUE) {
+		_E("Failed to create moment view layout");
+		evas_object_del(layout);
+		return NULL;
+	}
 
-        evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-        evas_object_size_hint_align_set(layout, EVAS_HINT_FILL, EVAS_HINT_FILL);
-        ad->moment_view_battery_number_layout = layout;
-        elm_object_part_content_set(ad->moment_view_battery_layout,"image.number.swallow",layout);
-        evas_object_show(layout);
+	evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	evas_object_size_hint_align_set(layout, EVAS_HINT_FILL, EVAS_HINT_FILL);
+	ad->moment_view_battery_number_layout = layout;
+	elm_object_part_content_set(ad->moment_view_battery_layout,"image.number.swallow",layout);
+	evas_object_show(layout);
 
-        return layout;
+	return layout;
 }*/
 
 Evas_Object *windicator_moment_view_battery_layout_create(void *data)
 {
-        struct appdata *ad =(struct appdata *)data;
-        retv_if(ad == NULL, NULL);
+	struct appdata *ad = (struct appdata *)data;
+	retv_if(ad == NULL, NULL);
 
-        _I("moment battery initialize");
-        int battery_level = 50;
+	_I("moment battery initialize");
+	int battery_level = 50;
 
-        Evas_Object *layout = elm_layout_add(ad->moment_view_win);
-        retv_if(layout == NULL, NULL);
+	Evas_Object *layout = elm_layout_add(ad->moment_view_win);
+	retv_if(layout == NULL, NULL);
 
-        char full_path[PATH_MAX] = { 0, };
-        _get_resource(EDJ_FILE, full_path, sizeof(full_path));
-        _D("full_path:%s",full_path);
+	char full_path[PATH_MAX] = { 0, };
+	_get_resource(EDJ_FILE, full_path, sizeof(full_path));
+	_D("full_path:%s", full_path);
 
-        if(elm_layout_file_set(layout, full_path, "windicator/moment_view/battery") != EINA_TRUE) {
-        	_E("Failed to create moment view layout");
-                evas_object_del(layout);
-                return NULL;
-        }
-        ad->moment_view_battery_layout = layout;
+	if (elm_layout_file_set(layout, full_path, "windicator/moment_view/battery") != EINA_TRUE) {
+		_E("Failed to create moment view layout");
+		evas_object_del(layout);
+		return NULL;
+	}
+	ad->moment_view_battery_layout = layout;
 
-        ad->battery_level = battery_level;
+	ad->battery_level = battery_level;
 
-        evas_object_resize(layout, ad->win_w, ad->win_h);
-
-//      wnidicator_moment_view_battery_image_create(ad);
-
-        evas_object_show(layout);
-
- //       windicator_moment_view_battery_number_create(data);
-
-        _I("moment battery initialize END %x",ad->moment_view_battery_layout);
-        return layout;
+	evas_object_resize(layout, ad->win_w, ad->win_h);
+	evas_object_show(layout);
+	_I("moment battery initialize END %x", ad->moment_view_battery_layout);
+	return layout;
 }
 
 Evas_Object *windicator_dynamic_layout_for_moment_view_create(Evas_Object *parent, void *data)
 {
-        retv_if(parent == NULL, NULL);
-        _I("moment dynamic view initialize");
-        struct appdata *ad = (struct appdata *)data;
-        retv_if(ad == NULL, NULL);
+	retv_if(parent == NULL, NULL);
+	_I("moment dynamic view initialize");
+	struct appdata *ad = (struct appdata *)data;
+	retv_if(ad == NULL, NULL);
 
-        Evas_Object *layout = elm_layout_add(parent);
-        retv_if(layout == NULL, NULL);
+	Evas_Object *layout = elm_layout_add(parent);
+	retv_if(layout == NULL, NULL);
 
-        char full_path[PATH_MAX] = { 0, };
-        _get_resource(EDJ_FILE, full_path, sizeof(full_path));
-        _D("full_path:%s",full_path);
+	char full_path[PATH_MAX] = { 0, };
+	_get_resource(EDJ_FILE, full_path, sizeof(full_path));
+	_D("full_path:%s", full_path);
 
 #ifdef _TIZEN_3G_DISABLE
-        if(elm_layout_file_set(layout, full_path, "windicator/dynamic/layout/bt") != EINA_TRUE) {
-                _E("Failed to create dynamic layout");
-                evas_object_del(layout);
-                return NULL;
-        }
+	if (elm_layout_file_set(layout, full_path, "windicator/dynamic/layout/bt") != EINA_TRUE) {
+		_E("Failed to create dynamic layout");
+		evas_object_del(layout);
+		return NULL;
+	}
 #else
-        if(elm_layout_file_set(layout, full_path, "windicator/dynamic/layout/3g") != EINA_TRUE) {
-                _E("Failed to create dynamic layout");
-                evas_object_del(layout);
-                return NULL;
-        }
+	if (elm_layout_file_set(layout, full_path, "windicator/dynamic/layout/3g") != EINA_TRUE) {
+		_E("Failed to create dynamic layout");
+		evas_object_del(layout);
+		return NULL;
+	}
 #endif
-        /* initialize battery icon */
-        ad->moment_view_battery_icon = windicator_battery_icon_create(layout, ad);
-        if(ad->moment_view_battery_icon == NULL) {
-                _E("Failed to create battery icon");
+	/* initialize battery icon */
+	ad->moment_view_battery_icon = windicator_battery_icon_create(layout, ad);
+	if (ad->moment_view_battery_icon == NULL) {
+		_E("Failed to create battery icon");
 			}
 
-        elm_object_part_content_set(parent, "sw.dynamic", layout);
-        _I("moment dynamic view initialize END");
-        return layout;
+	elm_object_part_content_set(parent, "sw.dynamic", layout);
+	_I("moment dynamic view initialize END");
+	return layout;
 }
 
 Evas_Object *windicator_moment_view_layout_create(void *data)
 {
-        struct appdata *ad =(struct appdata *)data;
-        retv_if(ad == NULL, NULL);
+	struct appdata *ad = (struct appdata *)data;
+	retv_if(ad == NULL, NULL);
 
-        _I("moment view initialize");
+	_I("moment view initialize");
 
-        Evas_Object *layout = elm_layout_add(ad->moment_view_win);
-        retv_if(layout == NULL, NULL);
+	Evas_Object *layout = elm_layout_add(ad->moment_view_win);
+	retv_if(layout == NULL, NULL);
 
-        char full_path[PATH_MAX] = { 0, };
-        _get_resource(EDJ_FILE, full_path, sizeof(full_path));
-        _D("full_path:%s",full_path);
+	char full_path[PATH_MAX] = { 0, };
+	_get_resource(EDJ_FILE, full_path, sizeof(full_path));
+	_D("full_path:%s", full_path);
 
-        if(elm_layout_file_set(layout, full_path, "windicator/moment_view/full") != EINA_TRUE) {
-        		_E("Failed to create moment view layout");
-                evas_object_del(layout);
-                return NULL;
-        }
+	if (elm_layout_file_set(layout, full_path, "windicator/moment_view/full") != EINA_TRUE) {
+		_E("Failed to create moment view layout");
+		evas_object_del(layout);
+		return NULL;
+	}
 
-        evas_object_resize(layout, ad->win_w, ad->win_h);
+	evas_object_resize(layout, ad->win_w, ad->win_h);
 
-        /* create dynamic layout */
-        ad->moment_view_dynamic_layout = windicator_dynamic_layout_for_moment_view_create(layout, ad);
-        if(ad->moment_view_dynamic_layout == NULL) {
-                _E("Failed to create dynamic layout");
-        }
+	/* create dynamic layout */
+	ad->moment_view_dynamic_layout = windicator_dynamic_layout_for_moment_view_create(layout, ad);
+	if (ad->moment_view_dynamic_layout == NULL) {
+		_E("Failed to create dynamic layout");
+		return NULL;
+	}
 #ifndef _TIZEN_3G_DISABLE
-        /* create rssi icon */
-        ad->moment_view_rssi_icon = windicator_rssi_icon_create(ad->moment_view_dynamic_layout, ad);
-        if(ad->moment_view_rssi_icon == NULL) {
-                _E("Failed to create RSSI icon for moment view");
-                return NULL;
-        }
+	/* create rssi icon */
+	ad->moment_view_rssi_icon = windicator_rssi_icon_create(ad->moment_view_dynamic_layout, ad);
+	if (ad->moment_view_rssi_icon == NULL) {
+		_E("Failed to create RSSI icon for moment view");
+		return NULL;
+	}
 #endif
-        /* create connection icon */
-        ad->moment_view_connection_icon = windicator_connection_icon_create(ad->moment_view_dynamic_layout, ad);
-        if(ad->moment_view_connection_icon == NULL) {
-                _E("Failed to create CONNECTION icon for moment view");
-                return NULL;
-        }
+	/* create connection icon */
+	ad->moment_view_connection_icon = windicator_connection_icon_create(ad->moment_view_dynamic_layout, ad);
+	if (ad->moment_view_connection_icon == NULL) {
+		_E("Failed to create CONNECTION icon for moment view");
+		return NULL;
+	}
 
-        evas_object_show(layout);
+	evas_object_show(layout);
 
-        _I("moment view initialize END");
-        return layout;
+	_I("moment view initialize END");
+	return layout;
 
 }
 
 windicator_error_e windicator_moment_view_init(void *data)
 {
-        struct appdata *ad = (struct appdata *)data;
-        retv_if(ad == NULL, WINDICATOR_ERROR_INVALID_PARAMETER);
+	struct appdata *ad = (struct appdata *)data;
+	retv_if(ad == NULL, WINDICATOR_ERROR_INVALID_PARAMETER);
 
-        /* create moment view window */
-        ad->moment_view_win = windicator_util_win_create("__MOMENT_VIEW__", EINA_TRUE, TYPE_MOMENT_VIEW);
-        retv_if(ad->moment_view_win == NULL, WINDICATOR_ERROR_FAIL);
+	/* create moment view window */
+	ad->moment_view_win = windicator_util_win_create("__MOMENT_VIEW__", EINA_TRUE, TYPE_MOMENT_VIEW);
+	retv_if(ad->moment_view_win == NULL, WINDICATOR_ERROR_FAIL);
 
-        windicator_moment_view_battery_layout_create(ad);
+	windicator_moment_view_battery_layout_create(ad);
 
-        /* create moment view layout */
-        ad->moment_view_layout = windicator_moment_view_layout_create(ad);
-        retv_if(ad->moment_view_layout == NULL, WINDICATOR_ERROR_FAIL);
+	/* create moment view layout */
+	ad->moment_view_layout = windicator_moment_view_layout_create(ad);
+	retv_if(ad->moment_view_layout == NULL, WINDICATOR_ERROR_FAIL);
 
-        evas_object_resize(ad->moment_view_win, ad->win_w, ad->moment_view_h);
-        ad->moment_view_evas = evas_object_evas_get(ad->moment_view_win);
-        ad->moment_view_ee = ecore_evas_ecore_evas_get(ad->moment_view_evas);
+	evas_object_resize(ad->moment_view_win, ad->win_w, ad->moment_view_h);
+	ad->moment_view_evas = evas_object_evas_get(ad->moment_view_win);
+	ad->moment_view_ee = ecore_evas_ecore_evas_get(ad->moment_view_evas);
 
-        if(WINDICATOR_ERROR_OK != windicator_util_win_notification_property_set(ad->moment_view_win)) {
-                _E("Failed to set window notification property");
-        }
+	if (WINDICATOR_ERROR_OK != windicator_util_win_notification_property_set(ad->moment_view_win)) {
+		_E("Failed to set window notification property");
+	}
 
-        elm_win_prop_focus_skip_set(ad->moment_view_win, EINA_TRUE);
-        return WINDICATOR_ERROR_OK;
+	elm_win_prop_focus_skip_set(ad->moment_view_win, EINA_TRUE);
+	return WINDICATOR_ERROR_OK;
 }
